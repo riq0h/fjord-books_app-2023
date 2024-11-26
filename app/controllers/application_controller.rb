516004888867b0a997de3dc44222854abe700649
Rikuoh <mail@riq0h.jp>
2024-11-26 16:24:15 +0900

    重複していた記述を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  around_action :switch_locale, default_url_options
+  around_action :switch_locale
+
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,6 @@ Bundler.require(*Rails.groups)
 
 module BooksApp
   class Application < Rails::Application
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    config.i18n.default_locale = :ja
-    config.i18n.available_locales = [:ja, :en]
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = :ja
     config.i18n.available_locales = [:ja, :en]
     # Initialize configuration defaults for originally generated Rails version.
