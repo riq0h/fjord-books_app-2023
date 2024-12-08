2ccabce5ac16da30bdc086ed4d6a8682e3c473ed
Rikuoh <mail@riq0h.jp>
2024-12-08 11:59:40 +0900

    i18n_pluralizeの修正および翻訳ファイルの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show]
   resources :books
-  resources :users
   root 'books#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
