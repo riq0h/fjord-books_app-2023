dbf5c001653f65891ce1fc3c61b56cb858d293ac
Rikuoh Tsujitani <mail@riq0h.jp>
2024-12-09 16:54:23 +0900

    完成

--- app/models/report.rb
+++ app/models/report.rb
@@ -7,11 +7,29 @@ class Report < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  has_many :mentions, dependent: :destroy
+  has_many :mentioning_reports, through: :mentions, source: :mentioned_report
+  has_many :reverse_of_mentions, class_name: 'Mention', foreign_key: 'mentioned_report_id', dependent: :destroy,
+                                 inverse_of: 'mentioned_report'
+  has_many :mentioned_reports, through: :reverse_of_mentions, source: :report
+  after_save :create_mention
+
   def editable?(target_user)
     user == target_user
   end
 
   def created_on
     created_at.to_date
+  end
+
+  def create_mention
+    ids = content.scan(%r{http://localhost:3000/reports/(\d+)}).flatten.uniq
+    mentioned_report_ids = Report.where(id: ids).where.not(id:).pluck(:id)
+    mentions.destroy_all
+    return if mentioned_report_ids.blank?
+
+    mentioned_report_ids.each do |id|
+      mentions.create(mentioned_report_id: id)
+    end
   end
 end
