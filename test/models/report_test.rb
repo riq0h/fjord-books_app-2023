eb1f92372085ee780c1b979f84d68997b3c4bc03
Rikuoh Tsujitani <mail@riq0h.jp>
2024-12-23 11:11:19 +0900

    テストコードの改善: メソッド名表記の修正、save_mentionsテストの強化、バリデーションテストの削除、その他確認工程を追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  test 'editable?' do
+  test '#editable?' do
     alice = users(:alice)
     bob = users(:bob)
     report = Report.new(user: alice)
@@ -11,7 +11,7 @@ class ReportTest < ActiveSupport::TestCase
     assert_not report.editable?(bob)
   end
 
-  test 'created_on' do
+  test '#created_on' do
     datetime = DateTime.new(1993, 2, 24, 12, 30, 45)
     date = Date.new(1993, 2, 24)
     report = Report.new(created_at: datetime)
@@ -23,42 +23,48 @@ class ReportTest < ActiveSupport::TestCase
     mentioning_report = reports(:report_id2)
     mentioning_report.content = 'http://localhost:3000/reports/1を読んだ'
     mentioning_report.save
-    assert mentioning_report.mentioning_reports.exists?
-    mentioning_report.mentioning_reports.each do |report|
-      assert_equal mentioned_report, report
-    end
+    assert ReportMention.exists?(mentioning_id: mentioning_report.id, mentioned_id: mentioned_report.id)
   end
 
   test 'save_mentions when update report' do
-    mentioned_report = reports(:report_id1)
-    mentioning_report = reports(:report_id2)
+    mentioned_report1 = reports(:report_id1)
+    mentioned_report2 = reports(:report_id2)
+    mentioning_report = reports(:report_id3)
+
+    # 既存のmentionを作成
     mentioning_report.update(content: 'http://localhost:3000/reports/1を読んだ')
-    assert mentioning_report.mentioning_reports.exists?
-    mentioning_report.mentioning_reports.each do |report|
-      assert_equal mentioned_report, report
-    end
+
+    # 新しいmentionを追加し、既存のmentionを維持
+    mentioning_report.update(content: 'http://localhost:3000/reports/1とhttp://localhost:3000/reports/2を読んだ')
+    assert ReportMention.exists?(mentioning_id: mentioning_report.id, mentioned_id: mentioned_report1.id)
+    assert ReportMention.exists?(mentioning_id: mentioning_report.id, mentioned_id: mentioned_report2.id)
+
+    # 既存のmentionを削除
+    mentioning_report.update(content: 'http://localhost:3000/reports/2のみ読んだ')
+    assert_not ReportMention.exists?(mentioning_id: mentioning_report.id, mentioned_id: mentioned_report1.id)
+    assert ReportMention.exists?(mentioning_id: mentioning_report.id, mentioned_id: mentioned_report2.id)
   end
 
   test 'save_mentions when destroy report' do
     mentioning_report = reports(:report_id2)
-    mentioning_report.content = 'http://localhost:3000/reports/1を読んだ'
-    mentioning_report.save
-    assert ReportMention.exists?
+    mentioned_report = reports(:report_id1)
+    mentioning_report.update(content: 'http://localhost:3000/reports/1を読んだ')
+    assert ReportMention.exists?(mentioning_id: mentioning_report.id, mentioned_id: mentioned_report.id)
     mentioning_report.destroy
-    assert ReportMention.all.blank?
+    assert_not ReportMention.exists?(mentioning_id: mentioning_report.id, mentioned_id: mentioned_report.id)
   end
 
   test 'save_mentions case mentioning not exist report' do
     mentioning_report = reports(:report_id2)
     mentioning_report.content = 'http://localhost:3000/reports/999を読んだ'
     mentioning_report.save
-    assert ReportMention.all.blank?
+    assert_not ReportMention.exists?(mentioning_id: mentioning_report.id)
   end
 
   test 'save_mentions case mentioning self report' do
     mentioning_report = reports(:report_id2)
     mentioning_report.content = 'めちゃくちゃhttp://localhost:3000/reports/2を読んだ'
     mentioning_report.save
-    assert ReportMention.all.blank?
+    assert_not ReportMention.exists?(mentioning_id: mentioning_report.id, mentioned_id: mentioning_report.id)
   end
 end
--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -24,33 +24,8 @@ class ReportsTest < ApplicationSystemTestCase
     fill_in '内容', with: @report.content
     click_on '登録する'
     assert_text '日報が作成されました。'
-    click_on '日報の一覧に戻る'
-  end
-  test 'should error create report in nothing title and content' do
-    visit reports_url
-    click_on '日報の新規作成'
-    click_on '登録する'
-    assert_text '2件のエラーがあるため、この日報は保存できませんでした:'
-    assert_text 'タイトルを入力してください'
-    assert_text '内容を入力してください'
-    click_on '日報の一覧に戻る'
-  end
-  test 'should error create report in nothing title' do
-    visit reports_url
-    click_on '日報の新規作成'
-    fill_in '内容', with: @report.content
-    click_on '登録する'
-    assert_text '1件のエラーがあるため、この日報は保存できませんでした:'
-    assert_text 'タイトルを入力してください'
-    click_on '日報の一覧に戻る'
-  end
-  test 'should error create report in nothing content' do
-    visit reports_url
-    click_on '日報の新規作成'
-    fill_in 'タイトル', with: @report.title
-    click_on '登録する'
-    assert_text '1件のエラーがあるため、この日報は保存できませんでした:'
-    assert_text '内容を入力してください'
+    assert_text @report.title
+    assert_text @report.content
     click_on '日報の一覧に戻る'
   end
 
@@ -63,59 +38,15 @@ class ReportsTest < ApplicationSystemTestCase
     fill_in '内容', with: new_content
     click_on '更新する'
     assert_text '日報が更新されました。'
+    assert_text new_title
+    assert_text new_content
     click_on '日報の一覧に戻る'
-  end
-  test 'should error update Report in nothing title and content' do
-    visit report_url(@report)
-    click_on 'この日報を編集'
-    new_title = ''
-    new_content = ''
-    fill_in 'タイトル', with: new_title
-    fill_in '内容', with: new_content
-    click_on '更新する'
-    assert_text '2件のエラーがあるため、この日報は保存できませんでした:'
-    assert_text 'タイトルを入力してください'
-    assert_text '内容を入力してください'
-    click_on '日報の一覧に戻る'
-  end
-  test 'should error update Report in nothing title' do
-    visit report_url(@report)
-    click_on 'この日報を編集'
-    new_title = ''
-    new_content = '内容を変更'
-    fill_in 'タイトル', with: new_title
-    fill_in '内容', with: new_content
-    click_on '更新する'
-    assert_text '1件のエラーがあるため、この日報は保存できませんでした:'
-    assert_text 'タイトルを入力してください'
-    click_on '日報の一覧に戻る'
-  end
-  test 'should error update Report in nothing content' do
-    visit report_url(@report)
-    click_on 'この日報を編集'
-    new_title = 'タイトルを変更'
-    new_content = ''
-    fill_in 'タイトル', with: new_title
-    fill_in '内容', with: new_content
-    click_on '更新する'
-    assert_text '1件のエラーがあるため、この日報は保存できませんでした:'
-    assert_text '内容を入力してください'
-    click_on '日報の一覧に戻る'
-  end
-  test 'should not update Report' do
-    bob_report = reports(:bob_report)
-    visit report_url(bob_report)
-    assert_no_text 'この日報を編集'
   end
 
   test 'should destroy Report' do
     visit report_url(@report)
     click_on 'この日報を削除'
     assert_text '日報が削除されました。'
-  end
-  test 'should not destroy Report' do
-    bob_report = reports(:bob_report)
-    visit report_url(bob_report)
-    assert_no_text 'この日報を削除'
+    assert_no_text @report.title
   end
 end
